7b6d1f4cc441a3202cccb29e7e535789b35baffc
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2020-12-26 19:06:11 +0100

    Tree without randomness

--- main.rs
+++ main.rs
@@ -1,7 +1,39 @@
+fn branch(n: i32, i: i32) -> String {
+    let mut result = String::from("");
+    for _ in 0..(n - i) {
+        result.push_str(" ");
+    }
+    for _ in 0..(2 * i + 1) {
+        result.push_str("x");
+    }
+    result
+}
+
+fn star(n: i32) -> String {
+    let mut result = String::from("");
+    for _ in 0..n {
+        result.push_str(" ");
+    }
+    result.push_str("+");
+    result
+}
+
+fn trunk(n : i32) -> String {
+    let mut result = String::from("");
+    for _ in 0..(n - 1) {
+        result.push_str(" ");
+    }
+    result.push_str("|||");
+    result
+}
+
 fn get_tree(n: i32) -> Vec<String> {
     let mut output = Vec::new();
-    output.push(String::from(" + "));
-    output.push(String::from("|||"));
+    output.push(star(n));
+    for i in 0..n {
+        output.push(branch(n, i));
+    }
+    output.push(trunk(n));
     output
 }
 
